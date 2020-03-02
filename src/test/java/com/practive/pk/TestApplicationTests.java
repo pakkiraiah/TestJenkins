17ec3b53267004306f1986d960bcab8b1e43dedf
pakkiraiah-kandi-dxc <pakkiraiah.kandi-dxc@hpe.com>
2020-03-02 21:30:37 +0530

    Test

---
+++ src/test/java/com/practive/pk/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practive.pk;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
